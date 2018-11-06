bd1aaf6485b2e4211462c05aeeae7ddbbc280497
tahorst <thorst@stanford.edu>
2018-11-05 21:55:11 -0800

    Add moving average distribution plots

--- src/analysis.py
+++ src/analysis.py
@@ -7,10 +7,12 @@ Usage (run from project directory):
 
 import os
 
+import numpy as np
+
 from util import load_wigs, load_genome, plot_distribution, OUTPUT_DIR
 
 
-def plot_read_distributions(genes, starts, ends, reads):
+def plot_read_distributions(genes, starts, ends, reads, desc):
     '''
     Plot read distributions for each gene.
 
@@ -20,17 +22,18 @@ def plot_read_distributions(genes, starts, ends, reads):
         ends (array of int): end position for each gene
         reads (2D array of float): reads for each strand at each position
             dims (strands x genome length)
+        desc (str): description of the data, becomes directory for output
     '''
 
     # Check for output directory
-    out_dir = os.path.join(OUTPUT_DIR, 'distributions')
+    out_dir = os.path.join(OUTPUT_DIR, desc)
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
     # Save a distribution for each gene in the genome
     for gene, start, end in zip(genes, starts, ends):
         out = os.path.join(out_dir, '{}.png'.format(gene))
-        plot_distribution(gene, start, end, reads, out)
+        plot_distribution(gene, start, end, ma, out)
 
 
 if __name__ == '__main__':
@@ -38,4 +41,12 @@ if __name__ == '__main__':
     reads = load_wigs()
     genes, _, starts, ends = load_genome()
 
-    plot_read_distributions(genes, starts, ends, reads)
+    # Raw read distributions
+    plot_read_distributions(genes, starts, ends, reads, 'distributions')
+
+    # Moving average distributions - becomes more Gaussian like
+    ma = np.zeros_like(reads)
+    window = 11
+    for i, strand in enumerate(reads):
+        ma[i, (window-1)//2:-(window-1)//2] = np.convolve(strand, np.ones((window,))/window, 'valid')
+    plot_read_distributions(genes, starts, ends, ma, 'ma_distributions')
