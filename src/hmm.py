ae312faaabff4819c901400b06455980e67c5da1
tahorst <thorst@stanford.edu>
2018-11-15 12:18:04 -0800

    Prepare hmm for parameter searching

--- src/hmm.py
+++ src/hmm.py
@@ -6,6 +6,20 @@ Usage:
 
 Output:
     Saves estimates for transcription unit assignments in output/hmm_assignments/
+
+Parameters to search:
+    n_levels (int): number of levels to assign, varies depending on genes in a region
+    trans_prob_up (float): value between 0 and 1 for the transition probability to a
+        spiked level
+    trans_prob_down (float): value between 0 and 1 for the transition probability to
+        a gene level
+    window (int): moving average window for smoothing inputs
+    expanded (bool): True if using wider feature set (within window surrounding point,
+        False if only the single point
+    n_iter (int): number of iterations for the GaussianHMM method
+
+TODO:
+    Select best assignment based on score between different seeds
 '''
 
 from __future__ import division
@@ -18,21 +32,26 @@ import numpy as np
 from util import load_genome, load_wigs, load_region_reads, get_region_info, get_n_regions, plot_reads, OUTPUT_DIR
 
 
+np.warnings.filterwarnings('ignore')
 VERBOSE = True
 
 
 if __name__ == '__main__':
     reads = load_wigs()
     genes, _, starts, ends = load_genome()
-    window = 21
-    n_iters = 10
+
+    # Parameters
+    window = 3
+    expanded = False
+    n_seeds = 10
+    n_iter = 20
 
     fwd_strand = True
     for region in range(get_n_regions(fwd_strand)):
-        print(region)
+        print('\nRegion: {}'.format(region))
 
         # Information for region to be analyzed
-        x = load_region_reads(reads, region, fwd_strand, ma_window=window, expanded=False)
+        x = load_region_reads(reads, region, fwd_strand, ma_window=window, expanded=expanded)
         start, end, region_genes, region_starts, region_ends = get_region_info(
             region, fwd_strand, genes, starts, ends)
         n_levels = 2*len(region_genes) + 2  # Extra level for 0 reads and spiked reads
@@ -50,32 +69,36 @@ if __name__ == '__main__':
                 trans_mat[n, n:n+2] = [1-trans_prob_up, trans_prob_up]
         trans_mat[-1, -1] = 1
 
-        for it in range(n_iters):
-            # Fit model
-            hmm = GaussianHMM(n_components=n_levels, init_params='mc')
-            hmm.startprob_ = start_prob
-            hmm.transmat_ = trans_mat
-            hmm.fit(x)
-            labels = hmm.predict(x)
-            # log_prob = np.log(gmm.predict_proba(x))
-            means = hmm.means_.mean(axis=1)
-            score = hmm.score(x)
+        for it in range(n_seeds):
+            try:
+                # Fit model
+                hmm = GaussianHMM(n_components=n_levels, init_params='mc', n_iter=n_iter)
+                hmm.startprob_ = start_prob
+                hmm.transmat_ = trans_mat
+                hmm.fit(x)
+                labels = hmm.predict(x)
+                means = hmm.means_.mean(axis=1)
+                score = hmm.score(x)
 
-            if VERBOSE:
-                if hmm.monitor_.converged:
-                    print('Converged with score: {:.2f}'.format(score))
-                else:
-                    print('** Did not converge **')
+                if VERBOSE:
+                    if hmm.monitor_.converged:
+                        print('Iteration {:2d} converged in {:2d} steps with score: {:.2f}'.format(
+                            it, hmm.monitor_.iter, score))
+                    else:
+                        print('** Did not converge **')
 
-            # Output levels
-            levels = np.array([means[l] for l in labels])
+                # Output levels
+                levels = np.array([means[l] for l in labels])
 
-            # Plot output
-            ## Path setup
-            out_dir = os.path.join(OUTPUT_DIR, 'hmm_assignments')
-            if not os.path.exists(out_dir):
-                os.mkdir(out_dir)
+                # Plot output
+                ## Path setup
+                out_dir = os.path.join(OUTPUT_DIR, 'hmm_assignments')
+                if not os.path.exists(out_dir):
+                    os.mkdir(out_dir)
 
-            ## Raw level assignments
-            out = os.path.join(out_dir, '{}_iter_{}.png'.format(region, it))
-            plot_reads(start, end, genes, starts, ends, reads, fit=levels, path=out)
+                ## Raw level assignments
+                out = os.path.join(out_dir, '{}_iter_{}.png'.format(region, it))
+                plot_reads(start, end, genes, starts, ends, reads, fit=levels, path=out)
+            except Exception as e:
+                print(e)
+                continue
